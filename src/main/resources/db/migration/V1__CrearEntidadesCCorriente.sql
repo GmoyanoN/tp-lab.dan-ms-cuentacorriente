8f3dd5a7cfb6cba013d01814650edb46383f1feb
GmoyanoN <guilloxg22@gmail.com>
2021-05-25 18:08:59 -0300

    Esto es el SQL

---
+++ src/main/resources/db/migration/V1__CrearEntidadesCCorriente.sql
@@ -0,0 +1,40 @@
+CREATE TABLE forma_pago
+(
+    id          INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    observacion VARCHAR(100)
+);
+
+CREATE TABLE pago
+(
+    id            INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fecha_pago    DATETIME         NOT NULL,
+    forma_pago_id INTEGER UNSIGNED NOT NULL,
+    cliente_id    INTEGER UNSIGNED NOT NULL,
+    CONSTRAINT fk_forma_pago_pago FOREIGN KEY (forma_pago_id) REFERENCES forma_pago (id)
+);
+
+CREATE TABLE cheque
+(
+    id          INTEGER UNSIGNED NOT NULL PRIMARY KEY,
+    nro_cheque  INTEGER          NOT NULL,
+    fecha_cobro DATETIME         NOT NULL,
+    banco       VARCHAR(50)      NOT NULL,
+    CONSTRAINT fk_forma_pago_cheque FOREIGN KEY (id) REFERENCES forma_pago (id)
+);
+
+CREATE TABLE efectivo
+(
+    id         INTEGER UNSIGNED NOT NULL PRIMARY KEY,
+    nro_recibo INTEGER          NOT NULL,
+    CONSTRAINT fk_forma_pago_efectivo FOREIGN KEY (id) REFERENCES forma_pago (id)
+);
+
+CREATE TABLE transferencia
+(
+    id                   INTEGER UNSIGNED NOT NULL PRIMARY KEY,
+    cbu_origen           VARCHAR(22)      NOT NULL,
+    cbu_destino          VARCHAR(22)      NOT NULL,
+    codigo_transferencia LONG             NOT NULL,
+    CONSTRAINT fk_forma_pago_transferencia FOREIGN KEY (id) REFERENCES forma_pago (id)
+
+);

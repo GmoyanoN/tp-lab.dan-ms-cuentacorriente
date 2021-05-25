14837a42c2f5a51032c9126f03d0a707355a8e2d
GmoyanoN <guilloxg22@gmail.com>
2021-05-25 18:41:00 -0300

    Rests by Henry

--- src/main/java/utn/frsf/isi/dan/grupotp/tplab/danmscuentacorriente/rest/PagoRest.java
+++ src/main/java/utn/frsf/isi/dan/grupotp/tplab/danmscuentacorriente/rest/PagoRest.java
@@ -1,5 +1,15 @@
     package utn.frsf.isi.dan.grupotp.tplab.danmscuentacorriente.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import utn.frsf.isi.dan.grupotp.tplab.danmscuentacorriente.model.Pago;
+import utn.frsf.isi.dan.grupotp.tplab.danmscuentacorriente.service.PagoService;
+
+import java.util.List;
+
+    @RestController
+@RequestMapping("/api/pago")
 public class PagoRest {
     private final PagoService pagoService;
     @Autowired
